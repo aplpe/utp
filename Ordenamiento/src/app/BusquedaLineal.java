8099276ee8cfb2758fd7b1919ae934931179895b
Cesar <Cesar@Cesar-PC>
2015-10-21 01:11:20 -0500

    Se implementa la version mejorada de "Busqueda Lineal" donde se encuentra la primera aparicion del elemento a buscar.

--- Ordenamiento/src/app/BusquedaLineal.java
+++ Ordenamiento/src/app/BusquedaLineal.java
@@ -5,10 +5,21 @@ public class BusquedaLineal {
 	public BusquedaLineal(){
 	}
 	
-	public int busquedaLineal(int arreglo[], int dato){				
+	public int busquedaLinealTonta(int arreglo[], int dato){				
 		int posicion = -1;
 		
 		for (int i = 0; i <= arreglo.length; i++) {
+			if (arreglo[i] == dato) {
+				posicion = i;
+			}
+		}
+		return posicion;
+	}	
+	
+	public int busquedaLinealInteligente(int arreglo[], int dato){				
+		int posicion = -1;
+		
+		for (int i = 0; (i <= arreglo.length) && (posicion == -1); i++) {
 			if (arreglo[i] == dato) {
 				posicion = i;
 			}
